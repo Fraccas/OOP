2ca3f37ae0726ef40a72b56bca755d79fbb6fa58
Fraccas <daltonward1@yahoo.com>
2019-07-31 15:21:18 -0500

    Finished Lab DiceyBusiness

---
+++ DiceyBusiness/script.js
@@ -0,0 +1,65 @@
+let diceContainer = $('.dice-container');
+let diceCount = 0;
+let diceHolder = [];
+let diceUni = ['', '\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685'];
+
+
+// Button Clicks
+$('#genB').click(function () {
+    let d = new Dice(diceCount);
+    diceHolder.push(d);
+    diceCount++;
+});
+
+$('#rollB').click(function () {
+    for (d of diceHolder) {
+        d.Roll();
+    }
+});
+
+$('#sumB').click(function () {
+    let sum = diceHolder.reduce((acc, val) => {
+        return acc + val.num;
+    }, 0);
+    alert(`The total sum is ${sum}`);
+});
+
+// Dice Object
+class Dice {
+    constructor(dCount) {
+        this.id = dCount;
+        this.num = Math.floor(Math.random() * 6) + 1;
+        let dDiv = $(`<div class="diceDiv" id="d${this.id}">${diceUni[this.num]}</div>`);
+        diceContainer.append(dDiv);
+   
+        dDiv.click(function() { // Roll() undefined here
+            this.num = Math.floor(Math.random() * 6) + 1;
+            dDiv.text(diceUni[this.num]);
+
+            // Update array value for Sum to work properly
+            for (let di of diceHolder) {
+                if ('d' + di.id == this.id) {
+                    di.num = this.num;
+                }
+            }
+        });
+
+        dDiv.dblclick(function () {
+            // update removal in array
+            let count = 0; // number of index in array
+            for (let di of diceHolder) {
+                if ('d' + di.id == this.id) {
+                    diceHolder.splice(count, 1);
+                }
+                count++;
+            }
+            // remove div from page
+            dDiv.remove();
+        });
+    }
+
+    Roll() {
+        this.num = Math.floor(Math.random() * 6) + 1;
+        $('#d' + this.id).text(diceUni[this.num]);
+    }
+}
